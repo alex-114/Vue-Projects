8971a4f632e04ce7b46de6fb5f5b536bc859859f
BuildTools <unconfigured@null.spigotmc.org>
2022-06-21 18:21:49 -0400

    First Try

---
+++ src/components/Tasks.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :key="task.id" >
+  <div>v-for="task in tasks"</div>
+    <Task
+      @toggle-reminder="$emit('toggle-reminder', task.id)"
+      @delete-task="$emit('delete-task', task.id)"
+      :task="task"
+    />
+  </div>
+</template>
+
+<script>
+import Task from './Task'
+
+export default {
+  name: 'Tasks-t',
+  props: {
+    tasks: Array,
+  },
+  components: {
+    Task,
+  },
+  emits: ['delete-task', 'toggle-reminder'],
+}
+</script>
